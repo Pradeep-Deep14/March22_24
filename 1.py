135ecf48bf5a310e7e3c1fed03ee2880c16ae0fd
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-03-22 16:49:34 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+my_list=[1,2,3,4,5]
+for i in my_list:
+    if i==5:
+        break
+    print(i)
+
+else:
+    print("Logical Python")
+
+#break statement just breaks the statement
+#nothing will be printed.
